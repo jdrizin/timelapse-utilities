3be24d745b4724c5c0c506f0e5e7cd809c5dfb00
Josh Drizin <josh@respectablename.com>
2013-08-12 13:20:22 -0500

    initial commit. untested with multiple cameras
    but it ought to work

---
+++ camera.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2.7
+
+from subprocess import check_output, call
+import argparse
+from re import findall
+
+#define parsers.
+parser = argparse.ArgumentParser(description='Control two Canon dSLR cameras of the same model using the ownername camera feature. Requires the gphoto2 binary in your $PATH. Images download in the current working directory', 
+	epilog='This will prompt you for the camera name from those detected. Capture begins after you hit Enter. Your camera may not be able to sustain 5s between shots, so play around with the interval setting') 
+parser.add_argument('frames', default='1440', help='number of frames to capture')
+parser.add_argument('interval', default='5', help='interval to wait between frames in seconds')
+parser.add_argument('-c', action='store_const', dest=command, default='--capture-image-and-download', const='--capture-image', help='keep the images on the card. only enable this if you have a large enough memory card in your camera')
+args = parser.parse_args()
+
+# functions
+
+# returns a list of usb port IDs for use with gphoto2
+def findCameraID( lines ):
+	return(findall(r"(usb:...,...)", lines))
+
+# use this on elements from a list to return a (port,name) object
+def getCameraName( port ):
+	ownerText = check_output(['gphoto2', '--port', port, '--get-config', 'ownername'])
+	cameraName = findall(r"Current: (.+)\n", ownerText)[0]
+	return([port, cameraName])
+
+cameras = check_output(['gphoto2', '--auto-detect']) #get the output from gphoto2
+ids = findCameraID(cameras) #strip the usb:ids from raw gphoto2 output
+names = [getCameraName(id) for id in ids] #output a list of names
+
+print 'detected cameras: ', [name[1] for name in names] #list the cameras
+
+cameraName = raw_input('Please type in the camera name: ') #prompt for camera name
+
+portName = [name[0] for name in names if name[1] == cameraName][0] #extract usb port name
+
+call(['gphoto2', '--port', portName, args.command, '-F', args.frames, '-I', args.interval] )
